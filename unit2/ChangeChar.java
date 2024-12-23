01c6eee111d7239d91d72828c9da84a36b21750c
CodeDog1024 <9556564+Code-Dog1024@user.noreply.gitee.com>
2024-12-23 23:08:03 +0800

    2024/12/23

--- unit2/ChangeChar.java
+++ unit2/ChangeChar.java
@@ -8,8 +8,12 @@ public class ChangeChar {
 		// \n ： 换行符
         System.out.println("jack\nsmith\nmary");
 		// \\\\ ：一个 \\
-	    // \\" ： 一个 \"
+        System.out.println("C:\\Windows\\System32\\cmd.exe");
+	    // \\" ： 一个 \""
+        System.out.println("老韩说:\"要好好学习 Java\"");
 		// \\' ：一个 \'
+        System.out.println("老韩说:\'要好好学习 Java\'");
 		// \\r ：回车符
+        System.out.println("韩顺平教育\r北京");
 	}
 }
