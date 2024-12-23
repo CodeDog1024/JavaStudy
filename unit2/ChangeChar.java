a2d7c73dad050cb05333a60ebbe953167626d3ed
CodeDog1024 <9556564+Code-Dog1024@user.noreply.gitee.com>
2024-12-23 22:43:32 +0800

    test

---
+++ unit2/ChangeChar.java
@@ -0,0 +1,15 @@
+package unit2;
+
+public class ChangeChar {
+
+	public static void main(String[] args) {
+		// \t ：制表符
+        System.out.println("北京\t天津\t上海");
+		// \n ： 换行符
+        System.out.println("jack\nsmith\nmary");
+		// \\\\ ：一个 \\
+	    // \\" ： 一个 \"
+		// \\' ：一个 \'
+		// \\r ：回车符
+	}
+}
---
+++ unit2/Demo01.java
@@ -0,0 +1,7 @@
+package unit2;
+
+public class Demo01 {
+	public static void main(String[] args) {
+		System.out.println("Codedog is studying Java!");	
+	}
+}
